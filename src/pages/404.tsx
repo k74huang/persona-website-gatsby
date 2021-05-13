b9c2b89b4b7c6ac5152ea54cf175623671377ab8
Kai Huang <edwardthetrueblade2678@gmail.com>
2021-05-13 16:34:29 -0400

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import "../styles/global.css"
+import Layout from "../components/layout"
+import Post from "../components/post"
+
+const IndexPage: React.FC = () => (
+  <Layout title="This do be a bruh moment">
+    <Post>
+      <div className="text-grey text-justify">
+        Seems like you found my 404 page. I'd appreciate it if you could toss me
+        an email / DM me on Discord with how you got here so I can get that
+        fixed!
+      </div>
+    </Post>
+  </Layout>
+)
+
+export default IndexPage
